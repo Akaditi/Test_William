d992ce3415f908e0b3f424110da44067fccbfddc
Akaditi <akaditi02@outlook.com>
2017-04-25 15:32:26 +0100

    Add files via upload

---
+++ Helaria/Helaria/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Helaria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txttitle = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.txtVenue = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dgvlist = new System.Windows.Forms.DataGridView();
+            this.lblD = new System.Windows.Forms.Label();
+            this.lblT = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvlist)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(359, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(359, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(359, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Time";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(359, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Venue";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(359, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Date";
+            // 
+            // txttitle
+            // 
+            this.txttitle.Location = new System.Drawing.Point(493, 57);
+            this.txttitle.Name = "txttitle";
+            this.txttitle.Size = new System.Drawing.Size(176, 20);
+            this.txttitle.TabIndex = 6;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(493, 245);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(176, 20);
+            this.txtPrice.TabIndex = 7;
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(493, 196);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(176, 20);
+            this.txtTime.TabIndex = 8;
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(493, 149);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(176, 20);
+            this.txtDate.TabIndex = 9;
+            // 
+            // txtVenue
+            // 
+            this.txtVenue.Location = new System.Drawing.Point(493, 98);
+            this.txtVenue.Name = "txtVenue";
+            this.txtVenue.Size = new System.Drawing.Size(176, 20);
+            this.txtVenue.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(373, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 47);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Add ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(594, 305);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 47);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(484, 305);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(82, 47);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dgvlist
+            // 
+            this.dgvlist.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvlist.Location = new System.Drawing.Point(12, 46);
+            this.dgvlist.Name = "dgvlist";
+            this.dgvlist.ReadOnly = true;
+            this.dgvlist.Size = new System.Drawing.Size(341, 306);
+            this.dgvlist.TabIndex = 15;
+            this.dgvlist.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvlist_RowHeaderMouseClick);
+            // 
+            // lblD
+            // 
+            this.lblD.AutoSize = true;
+            this.lblD.Location = new System.Drawing.Point(675, 149);
+            this.lblD.Name = "lblD";
+            this.lblD.Size = new System.Drawing.Size(15, 13);
+            this.lblD.TabIndex = 16;
+            this.lblD.Text = "D";
+            this.lblD.Click += new System.EventHandler(this.lblD_Click);
+            // 
+            // lblT
+            // 
+            this.lblT.AutoSize = true;
+            this.lblT.Location = new System.Drawing.Point(677, 196);
+            this.lblT.Name = "lblT";
+            this.lblT.Size = new System.Drawing.Size(14, 13);
+            this.lblT.TabIndex = 16;
+            this.lblT.Text = "T";
+            this.lblT.Click += new System.EventHandler(this.lblT_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Helaria";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 473);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblT);
+            this.Controls.Add(this.lblD);
+            this.Controls.Add(this.dgvlist);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtVenue);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txttitle);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvlist)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txttitle;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.TextBox txtVenue;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dgvlist;
+        private System.Windows.Forms.Label lblD;
+        private System.Windows.Forms.Label lblT;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Helaria/Helaria/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Helaria
+{
+
+    public partial class Form1 : Form
+    {
+        int ID = 0;
+
+        string GetConnectionString()
+        {
+            return Helaria.Properties.Settings.Default.constr;
+        }
+
+        SqlConnection GetConnection()
+        {
+            string s = GetConnectionString();
+            return new SqlConnection(Helaria.Properties.Settings.Default.constr);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayData();
+        }
+
+        private void DisplayData()
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT Id, Title, Venue, Date, Time, Price FROM Helaria", GetConnection());
+            da.Fill(dt);
+            dgvlist.DataSource = dt;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txttitle.Text != "" && txtVenue.Text != "")
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO  Helaria (Title, Venue, Date, Time, Price)VALUES (@Title, @Venue, @Date, @Time, @Price)", GetConnection());
+                cmd.Parameters.AddWithValue("@Title", txttitle.Text);
+                cmd.Parameters.AddWithValue("@Venue", txtVenue.Text);
+                cmd.Parameters.AddWithValue("@Date", txtDate.Text);
+                cmd.Parameters.AddWithValue("@Time", txtTime.Text);
+                cmd.Parameters.AddWithValue("@Price", txtPrice.Text);
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+                txttitle.Text = "";
+                txtVenue.Text = "";
+                txtDate.Text = "";
+                txtPrice.Text = "";
+                txtTime.Text = "";
+
+                DisplayData();
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+            }
+        }
+
+        private void lblD_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            txtDate.Text = Convert.ToDateTime(dt).ToString("dd MMM yyyy");
+        }
+
+        private void lblT_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            txtTime.Text = Convert.ToDateTime(dt).ToString("hh:mm:ss");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (txttitle.Text != "" && txtVenue.Text != "")
+            {
+                SqlCommand cmd = new SqlCommand("UPDATE Helaria SET Title = @Title, Venue = @Venue, Date = @Date, Time = @Time, Price = @Price where id = @id", GetConnection());
+                cmd.Parameters.AddWithValue("@id", ID);
+                cmd.Parameters.AddWithValue("@Title", txttitle.Text);
+                cmd.Parameters.AddWithValue("@Venue", txtVenue.Text);
+                cmd.Parameters.AddWithValue("@Date", Convert.ToDateTime(txtDate.Text).ToString("dd MMM yyyy"));
+                cmd.Parameters.AddWithValue("@Time", Convert.ToDateTime(txtTime.Text).ToString("hh:mm:ss"));
+            
+            cmd.Parameters.AddWithValue("@Price", txtPrice.Text);
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+
+                txttitle.Text = "";
+                txtVenue.Text = "";
+                txtDate.Text = "";
+                txtPrice.Text = "";
+                txtTime.Text = "";
+
+                DisplayData();
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+            }
+        }
+
+        private void dgvlist_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            ID = Convert.ToInt32(dgvlist.Rows[e.RowIndex].Cells[0].Value.ToString());
+            txttitle.Text = dgvlist.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtVenue.Text = dgvlist.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtDate.Text = dgvlist.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtTime.Text = Convert.ToDateTime(dgvlist.Rows[e.RowIndex].Cells[4].Value).ToString("hh:mm:ss");
+            txtPrice.Text = dgvlist.Rows[e.RowIndex].Cells[5].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (ID != 0)
+            {
+                SqlCommand cmd = new SqlCommand("delete Helaria where id=@id", GetConnection());
+
+                cmd.Parameters.AddWithValue("@id", ID);
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+                txttitle.Text = "";
+                txtVenue.Text = "";
+                txtDate.Text = "";
+                txtPrice.Text = "";
+                txtTime.Text = "";
+
+                DisplayData();
+            }
+        }
+    }
+}
